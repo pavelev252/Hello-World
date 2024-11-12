1b0dcdad9d6201272a80ddaa5acd7d56cbd7ff4f
pavelev252 <105489081+pavelev252@users.noreply.github.com>
2024-11-12 22:52:40 +0500

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,9 +0,0 @@
-my_string = input()
-print(len(my_string),
-      my_string.upper(),
-      my_string.lower(),
-      ''.join(my_string.split()),
-      my_string[0], my_string[-1],
-      sep='\n')
-
-
